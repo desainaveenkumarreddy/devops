ff01e103e89386f2e9e0f25fd5f184641b184084
desainaveenkumarreddy <desainaveenkumarreddy@gmail.com>
2024-04-20 12:01:31 +0530

    example

---
+++ 1.Scripts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "Ramesh:: Hi Suresh"
+echo "Suresh:: hello Ramesh"
+echo "Suresh:: Fuckoff Ramesh"
+echo "Ramesh:: i will fuck ur sister badly Suresh"
