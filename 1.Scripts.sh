e09c58c614e5215ba0ce315fe657ea05aa31584d
desainaveenkumarreddy <desainaveenkumarreddy@gmail.com>
2024-04-20 12:14:01 +0530

    example

--- 1.Scripts.sh
+++ 1.Scripts.sh
@@ -5,5 +5,5 @@ PERSON2=Suresh
 
 echo "$PERSON1:: Hi $PERSON2"
 echo "$PERSON2:: hello $PERSON1"
-echo "$PERSON2:: Fuckoff $PERSON1"
-echo "$PERSON1:: i will fuck ur sister badly $PERSON2"
+echo "$PERSON1:: Fuckoff $PERSON2"
+echo "$PERSON2:: i will fuck ur sister badly $PERSON1"
