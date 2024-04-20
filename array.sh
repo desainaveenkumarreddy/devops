e33343120afd3a54d272866214e67b126521c9c1
desainaveenkumarreddy <desainaveenkumarreddy@gmail.com>
2024-04-20 13:09:22 +0530

    example

--- array.sh
+++ array.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-Movies=("xxx" "xnxx" "xvideos" "kamasutra")#Space is important between arrays
+Movies=("xxx" "xnxx" "xvideos" "kamasutra")
 
 #size of above array is 4
 #index of above array is 0,1,2,3
