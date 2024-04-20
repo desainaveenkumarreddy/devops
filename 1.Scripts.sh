e40a9ece2bf34c84f84fe6b4e2d84e1d60637489
desainaveenkumarreddy <desainaveenkumarreddy@gmail.com>
2024-04-20 12:25:41 +0530

    example

--- 1.Scripts.sh
+++ 1.Scripts.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-PERSON1=$1
-PERSON2=$2
+#PERSON1=$1
+#PERSON2=$2
 
-echo "$PERSON1:: Hi $PERSON2"
-echo "$PERSON2:: hello $PERSON1"
-echo "$PERSON1:: Fuckoff $PERSON2"
-echo "$PERSON2:: i will fuck ur sister badly $PERSON1"
+#echo "$PERSON1:: Hi $PERSON2"
+#echo "$PERSON2:: hello $PERSON1"
+#echo "$PERSON1:: Fuckoff $PERSON2"
+#echo "$PERSON2:: i will fuck ur sister badly $PERSON1"
+
+
+
+Person1=$1
+Person2=$2   
+
+Echo "$Person1:: Hi $Person2, how are you"
+Echo "$Person2:: I am dng good, what about u $Person1"
+Echo "$Person1:: that's good to hear, where are u now ..? "
+Echo "$Person2:: I am in Bangalore and bye"
+Echo "$Person1:: ok bye ...Fuck off  $Person2"
