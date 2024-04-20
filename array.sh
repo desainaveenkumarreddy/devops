d99452f0bf086b08f0c0a1a128941774d174b2c7
desainaveenkumarreddy <desainaveenkumarreddy@gmail.com>
2024-04-20 13:03:18 +0530

    example

--- array.sh
+++ array.sh
@@ -1,18 +1,18 @@
 #!bin/bash
 
-Movies=("xxx" "xnxx" "xvideos" "kamasutra")
+Movies=("xxx","xnxx","xvideos","kamasutra")
 
 #size of above array is 4
 #index of above array is 0,1,2,3
 
 #list of arry aways starts with "0"
 
-echo "Movie1 is : ${Movie[0]}"
-echo "Movie1 is : ${Movie[1]}"
-echo "Movie1 is : ${Movie[2]}"
+echo "Movie1 is : ${Movies[0]}"
+echo "Movie1 is : ${Movies[1]}"
+echo "Movie1 is : ${Movies[2]}"
 
 #if u want ot list all array values that use "@"
 
 echo "All list of movies are below"
 
-echo "Movie1 is : ${Movie[@]}"
+echo "Movie1 is : ${Movies[@]}"
