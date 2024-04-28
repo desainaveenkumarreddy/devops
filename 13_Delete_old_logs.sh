a652ac15a87490b672abedf5312f08863de6672d
desainaveenkumarreddy <desainaveenkumarreddy@gmail.com>
2024-04-28 10:37:05 +0530

    delete files

---
+++ 13_Delete_old_logs.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+Source_Directory=/tmp/app-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $Source_Directory]
+then 
+    echo -e "$G Directory is present $N"
+else    
+    echo -e "$R Directory is not present,please create $N"
+    exit 1 
+fi
+
+Files=$(find $Source_Directory -name "*.logs" -mtime +14)
